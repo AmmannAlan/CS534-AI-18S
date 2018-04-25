a036825abb31929d588593ad48df2ec24627714e
Jiaming Nie <jiaming.nie13@gmail.com>
2018-04-24 20:22:55 -0400

    4-24

--- FinalProject/BoardGUI/boardUI.py
+++ FinalProject/BoardGUI/boardUI.py
@@ -38,6 +38,7 @@ class Filter(QtCore.QObject):
         return False
 
 class Ui_MainWindow(object):
+
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(1000, 800)
@@ -49,6 +50,8 @@ class Ui_MainWindow(object):
         self.graphicsView = QtWidgets.QGraphicsView(self.centralwidget)
         self.graphicsView.setGeometry(QtCore.QRect(0, 0, 760, 780))
         self.graphicsView.setObjectName("graphicsView")
+        self.graphicsView.setMinimumSize(QtCore.QSize(760, 780))
+        self.graphicsView.setMaximumSize(QtCore.QSize(760, 780))
 
         self.scene = QtWidgets.QGraphicsScene()
         #self.scene.addPixmap(QPixmap('C:\\Users\Jiaming Nie\Documents\GitHub\CS534-AI-18S\FinalProject\BoardGUI\source\background.jpg'))
@@ -153,34 +156,91 @@ class DisplayMW(QMainWindow):
         self.mouse_point.setGeometry(270, 270, PIECE, PIECE)
 
         # settings for the mouse
-        self.mouse_point.raise_()
-        self.setMouseTracking(True)
+        #self.mouse_point.raise_()
+        #self.setMouseTracking(True)
         self.AI_down = True
 
         self.chess_board = ChessBoard()
         self.pieces = [LaBel(self) for i in range(361)] # All the chess pieces , 19*19 chess board
 
+        ## Black go first
+        self.piece_now = BLACK
+        self.player_trun = True
+        self.step = 0
+
         for piece in self.pieces:
             piece.setVisible(True)  # Set Picture Visible
             piece.setScaledContents(True)  #
 
+        self.mouse_point.raise_()
+        self.setMouseTracking(True)
+        self.show()
+
     def mouseMoveEvent(self, event):
-        if event.x() <= 760 and event.y() <= 760:
+        if event.x() <= 760 and event.y() <= 780:
             self.mouse_point.move(event.x() - PIECE/2,event.y() - PIECE/2)
         #print("Moved")
 
     def mousePressEvent(self, event):
         if event.button() == Qt.LeftButton:
-            print("Pressed")
+            x,y = event.x(),event.y()
+            print("Pressed","x:",x,"y:",y)
+            i,j = self.coordinate_transform_pixel2map(x,y)
+            print("chess board i",i,"j ",j)
+
+            if not i is None and not j is None:
+                print("Both Not None")
+                #print(self.chess_board.get_xy_on_logic_state(i,j))
+
+                ### 以下这句是有问题的
+                temp_state = self.chess_board.get_xy_on_logic_state(i,j)
+                if temp_state == EMPTY:  # 棋子落在空白处
+                    #print("is Empty")
+
+                    self.draw(i, j)
+                    print("i",i,"j",j)
+                    #self.ai_down = False
+                    #board = self.chessboard.board()
+
+                    ## Create the AI thread
+                    #self.AI = AI(board)  # 新建线程对象，传入棋盘参数
+                    #self.AI.finishSignal.connect(self.AI_draw)  # 结束线程，传出参数
+                    #self.AI.start()  # run
+
 
     def coordinate_transform_map2pixel(self,i,j):
 
-        return
+        return MARGIN + j * GRID - PIECE / 2, MARGIN + i * GRID - PIECE / 2
+        
+
 
     def coordinate_transform_pixel2map(self,x,y):
 
+        i, j = int(round((y - MARGIN) / GRID)), int(round((x - MARGIN) / GRID))
+        # 有MAGIN, 排除边缘位置导致 i,j 越界
+        if i < 0 or i >= 19 or j < 0 or j >= 19:
+            return None, None
+        else:
+            return i, j
 
-        return
+    def draw(self,i,j):
+        x,y = self.coordinate_transform_map2pixel(i,j)
+        print("x",x,"y",y)
+        print("x should:",20+j*40,"y should",20+i*40)
+
+        if self.piece_now == BLACK:
+            # place black chess
+            self.pieces[self.step].setPixmap(self.ui.graphicsView.black)
+            self.piece_now = WHITE
+            self.chess_board.draw_xy(i,j,BLACK)
+        else:
+            self.pieces[self.step].setPixmap(self.ui.graphicsView.white)
+            self.piece_now = BLACK
+            self.chess_board.draw_xy(i,j,WHITE)
+
+        self.pieces[self.step].setGeometry(x,y-PIECE/2,PIECE,PIECE)
+        self.step += 1
+
 
 
 
